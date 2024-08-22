c911c276512f2a3fec325e3815378ad5515c7c9c
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-08-22 16:20:27 +0530

    Java Program to Reverse a String

---
+++ src/ReverseString.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String : ");
+        String s = sc.next();
+        System.out.println("Reversed String: "+reverse(s));
+
+    }
+    public static String reverse(String s) {
+        String rev="";
+        for (int i = s.length()-1; i >=0 ; i--) {
+            rev=rev+s.charAt(i);
+        }
+        return rev;
+    }
+}
