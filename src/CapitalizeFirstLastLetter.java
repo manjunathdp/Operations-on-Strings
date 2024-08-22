1c35e0c683a65d3a93bcc99cbbda59ad4905c245
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-08-23 00:12:26 +0530

    Java program to Capitalize the First and Last letter of Each Word of a String

---
+++ src/CapitalizeFirstLastLetter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CapitalizeFirstLastLetter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Sentence: ");
+        String s = sc.nextLine();
+        capitalize(s);
+    }
+
+    public static void capitalize(String s) {
+        String[] words = s.split(" ");
+        for (String word : words) {
+            System.out.print(word.toUpperCase().charAt(0) + word.substring(1, word.length() - 1) + word.toUpperCase().charAt(word.length() - 1) + " ");
+        }
+    }
+}
